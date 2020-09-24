e894abb1c1c583926a6287d41091c935becc868a
David Walker <dwalker@cellmark.co.uk>
2020-09-24 14:03:53 +0100

    Added data entry defaults for restricted option input

---
+++ src/NatureRecorder.Interpreter/Entities/Map.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NatureRecorder.Interpreter.Entities
+{
+    public class Map<K, V> : Dictionary<K, V> where V : IComparable
+    {
+        public K GetKey(V value)
+        {
+            return this.First(d => d.Value.CompareTo(value) == 0).Key;
+        }
+    }
+}
